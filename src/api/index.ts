b62b995d0e20a35e675a712e1fd8e50ba5d4f803
Veezean <veezean@hotmail.com>
2024-08-10 16:45:41 +0800

    全局异常处理优化

--- src/api/index.ts
+++ src/api/index.ts
@@ -63,7 +63,18 @@ api.interceptors.response.use(
     return Promise.resolve(response.data)
   },
   (error) => {
-
+    let message = error.message
+    if (message === 'Network Error') {
+      message = '后端网络故障'
+    }
+    else if (message.includes('timeout')) {
+      message = '接口请求超时'
+    }
+    else if (message.includes('Request failed with status code')) {
+      message = `接口${message.substr(message.length - 3)}异常`
+    }
+    ElMessage({
+      message,
       type: 'error',
     })
     return Promise.reject(error)
